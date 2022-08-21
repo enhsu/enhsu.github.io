091e29e6bba66e1edcbb8ff19f83df7995eed420
enhsu <enhsu79@gmail.com>
2022-08-22 06:35:06 +0800

    next.js static version

---
+++ _next/static/96ZQ0wzrX2MsU0mKRcx_j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/252f366e-362071c8e711dda3.js","static/css/08741df1b145377e.css","static/chunks/pages/index-0277aef992a73e4e.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
